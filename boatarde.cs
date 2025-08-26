92040ffeb4bdc9a60d78290c40fe79afe8444ef8
Alexandra Marques <prof.alexandra.marques@gmail.com>
2025-08-26 13:42:33 -0300

    Add files via upload

---
+++ boatarde.cs
@@ -0,0 +1,14 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite seu nome:");
+        string nome = Console.ReadLine();
+        Console.WriteLine ("Boa tarde "+nome);
+    }
+}
